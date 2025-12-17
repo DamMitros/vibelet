16a9463b08ab89b41d3c70c74b23e596f3772d4d
Damian Mitros <d.mitros.122@studms.ug.edu.pl>
2025-12-17 20:02:33 +0100

    Init project

---
+++ src/main/java/com/example/vibelet/VibeletApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibelet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vibelet/VibeletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibelet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
